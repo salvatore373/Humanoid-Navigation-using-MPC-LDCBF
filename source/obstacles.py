85926b88c1cc217d1c12dcb8793a244eccb381b3
EugenioBugli <eugeniobugli15@gmail.com>
2024-12-09 17:31:38 +0100

    scattered obs

--- source/obstacles.py
+++ source/obstacles.py
@@ -2,17 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
 from scipy.spatial import ConvexHull
-from sympy import Point, Polygon 
+from sympy import Point, Polygon
+import random
+
 
 def CreateConvexObstacle(n):
-    rnd_points = np.random.rand(n,2) # random_points inside the obstacle
+    mu_x = random.uniform(1, 20)
+    mu_y = random.uniform(1, 20)
+    std = 0.7
+    rnd_points_x = np.random.normal(mu_x, std, size=(n)) # random x coordinate
+    rnd_points_y = np.random.normal(mu_y, std, size=(n)) # random y coordinate
+    rnd_points = np.stack((rnd_points_x, rnd_points_y), axis=1) # random points
     hull = ConvexHull(rnd_points) # create a convex hull that contains the n random points
     vertices=[]
     for simplex in hull.simplices:
         vertices+=list(simplex)
     return rnd_points[hull.vertices]
 
-num_obs = 4
+num_obs = 20
 poly_list = []
 for j in range(0, num_obs):
     n = np.random.randint(3, 10) # num of random_points inside an obstacle
