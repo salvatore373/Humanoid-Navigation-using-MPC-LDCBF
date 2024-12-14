c05f4b500344c08835fa3f1cc4a6e22ef4105e60
damiano imola <damiano.imola@outlook.it>
2024-12-14 14:43:44 +0100

    some additional comments

--- Source/humanoid_2d_animation.py
+++ Source/humanoid_2d_animation.py
@@ -6,23 +6,25 @@ import math
 fig = plt.gcf()
 ax = fig.gca()
 
-
-start = np.random.randint(-10, 10, (3, 1))
-start = np.array([start[0], start[1], np.deg2rad(np.rad2deg(start[2])%360)])
-# start = np.array([start[0], start[1], np.array([3.14])])
-
-
-
-goal = np.random.randint(-10, 10, (3, 1))
-goal = np.array([goal[0], goal[1], np.deg2rad(np.rad2deg(goal[2])%360)])
-
+# ===== CONSTANTS =====
 step_size = 3
 foot_distance = 1
 
+# random initial position
+start = np.random.randint(-10, 10, (3, 1))
+start = np.array([start[0], start[1], np.deg2rad(np.rad2deg(start[2])%360)])
+
+# random goal position
+goal = np.random.randint(-10, 10, (3, 1))
+goal = np.array([goal[0], goal[1], np.deg2rad(np.rad2deg(goal[2])%360)])
+
+# initial left foot position
 left_foot = np.array([
     start[0] - foot_distance*np.sin(start[2]),
     start[1] + foot_distance*np.cos(start[2])
 ])
+
+# initial right foot position
 right_foot = np.array([
     start[0] + foot_distance*np.sin(start[2]),
     start[1] - foot_distance*np.cos(start[2])
@@ -46,13 +48,19 @@ draw_tick(start)
 plt.scatter(left_foot[0], left_foot[1], marker="o", color="green", label='left foot', s=100, alpha=0.7)
 
 # start marker right foot
-plt.scatter(right_foot[0], right_foot[1], marker="o", color="lightgreen", label='left foot', s=100, alpha=0.7)
+plt.scatter(right_foot[0], right_foot[1], marker="o", color="lightgreen", label='right foot', s=100, alpha=0.7)
 
 # goal marker
 plt.scatter(goal[0], goal[1], marker="o", color="royalblue", label='goal', s=300)
 
 for i in range(10):
     foot = 0 if i%2==0 else 1
+
+    # FIXME: make it a one-line expression
+    if i == 0:
+        step_size = step_size/2
+    else:
+        step_size = 3
 
     start = np.array([
         start[0] + step_size / 2 * np.cos(start[2]),
@@ -64,14 +72,12 @@ for i in range(10):
     draw_tick(start, alpha=1.0-i*0.1)
 
     if foot == 0:
-        print("LEFT")
         left_foot = np.array([
             left_foot[0] + step_size * np.cos(start[2]),
             left_foot[1] + step_size * np.sin(start[2])
         ])
         plt.scatter(left_foot[0], left_foot[1], marker="o", color="green", s=100, alpha=0.7)
     else:
-        print("RIGHT")
         right_foot = np.array([
             right_foot[0] + step_size * np.cos(start[2]),
             right_foot[1] + step_size * np.sin(start[2])
