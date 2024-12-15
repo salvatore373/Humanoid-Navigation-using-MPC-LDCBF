b293bd915bc695742a78378765d909e7490dbcfe
EugenioBugli <eugeniobugli15@gmail.com>
2024-12-15 15:33:17 +0100

    mpc rework but still need fixes

--- Source/diff_drive.py
+++ Source/diff_drive.py
@@ -27,7 +27,7 @@ class DifferentialDrive():
         self.ydot = self.v*cs.sin(self.theta)
         self.thetadot = self.w
         self.statedot = cs.vertcat(self.xdot, self.ydot, self.thetadot)
-        kinemodel = cs.Function("km", [self.state, self.controls], [self.statedot])
+        self.kinemodel = cs.Function("km", [self.state, self.controls], [self.statedot])
 
     def plot(self, state, h=0.1, w=0.05):
 
@@ -41,61 +41,3 @@ class DifferentialDrive():
 
         triangle = (rotation_matrix @ vert).T + np.array([[x, y]]) # rotate then translate
         plt.fill(triangle[:, 0], triangle[:, 1], color='seagreen')
-
-    def integration(self, X, U, type="Euler"): # gives me the continuos dynamics
-        if type=="Euler":
-            Xk1 = U[0]*cs.cos(X[2]) # Vk*cos(Thetak)
-            Yk1 = U[0]*cs.sin(X[2]) # Vk*sin(Thetak)
-            Thetak1 = U[1] # Wk
-            return cs.vertcat(Xk1, Yk1, Thetak1)
-    
-        # add later Runge Kutta
-
-    def mpc(self, goal, sampling_time):
-        # N : horizon for the MPC
-        # N_simul : simulation time
-
-        optim_prob = cs.Opti() 
-        p_opts, s_opts = {"ipopt.print_level": 0, "expand": True}, {}
-        optim_prob.solver("ipopt", p_opts, s_opts) # define the solver as ipopt (NLP solver)
-
-        # trajectory
-        u = np.zeros((self.m, self.N_simul))
-        x = np.zeros((self.n, self.N_simul+1))
-
-        # define the variables of the optimization problem
-        X = optim_prob.variable(self.n, self.N+1) # containter for all the states
-        U = optim_prob.variable(self.m, self.N) # containter for all the control inputs
-
-        x0 = optim_prob.parameter(self.n) # this is the initial state parameter which will be specified later (useful for MPC) with optim_prob.set_value(x0, value)
-        # definitions of constraints for our MPC problem
-        optim_prob.subject_to(X[:,0] == x0) # initial condition constraint
-        optim_prob.subject_to(X[:,self.N] == goal) # final condition constraint
-        for k in range(self.N):
-            optim_prob.subject_to(X[:,k+1] == X[:,k] + sampling_time*self.integration(X[:,k], U[:,k])) # related to integration: xk+1 = xk + sampling_time*f()
-        optim_prob.minimize(cs.sumsqr(U)) # cost function
-
-        iter_time = np.zeros(self.N_simul)
-        for k in range(self.N_simul): # at each instant of simulation I am looking forward N steps
-            init_time = time.time()
-            optim_prob.set_value(x0, x[:, k]) # now the initial condition of the step is the current state
-            print(x0)
-            solution = optim_prob.solve()
-            u[:,k] = solution.value(U[:,0]) # get the first control action from your predictions
-            u_pred = solution.value(U)
-            x_pred = solution.value(X)
-            optim_prob.set_initial(X, x_pred)
-            optim_prob.set_initial(U, u_pred)
-            x[:,k+1] = x[:,k] + sampling_time * self.integration(x[:,k], u[:,k]).full().squeeze(-1) # integration procedure
-            iter_time[k] = time.time() - init_time
-
-        print('Average computation time: ', np.mean(iter_time) * 1000, ' ms')
-        return x
-
-diff = DifferentialDrive(n=3, m=2, N=100, N_simul=300)
-goal = (2, 3, cs.pi)
-x = diff.mpc(goal, sampling_time=0.01)
-diff.plot(state=[1, 2, -cs.pi/2])
-plt.plot(x[0,:], x[1,:])
-plt.plot(goal[0], goal[1], marker='o')
-plt.show()
---
+++ Source/mpc.py
@@ -0,0 +1,100 @@
+import time
+import numpy as np
+import casadi as cs
+import matplotlib.pyplot as plt
+
+class Mpc():
+
+    def __init__(self, n, m, N, N_simul, sampling_time, goal=None, reference=None):
+        self.state_dim = n
+        self.control_dim = m
+        self.N = N # horizon MPC 
+        self.N_simul = N_simul # simulation
+        self.delta_t = sampling_time
+        self.goal = goal
+        self.reference = reference
+
+        self.optim_prob = cs.Opti()
+        p_opts, s_opts = {"ipopt.print_level": 0, "expand": True}, {}
+        self.optim_prob.solver("ipopt", p_opts, s_opts) # define the solver as ipopt (NLP solver)
+
+        # predicted trajectory over the simulation
+        self.u = np.zeros((self.control_dim, self.N_simul))
+        self.x = np.zeros((self.state_dim, self.N_simul+1))
+
+        # trajectory predicted during each step from the actual state to the horizon
+        self.X = self.optim_prob.variable(self.state_dim, self.N+1)
+        self.U = self.optim_prob.variable(self.control_dim, self.N) 
+
+        # initial state (this value will be update at each step)
+        self.x0 = self.optim_prob.parameter(self.state_dim)
+
+        cost_function = cs.sumsqr(self.U)
+        if self.reference is not None:
+            reference_cost = 0
+            for k in range(self.N):
+                reference_cost += self.x[1:, k] - self.reference[:, k] # xy trajectory
+            terminal_cost = self.x[1:, N] - self.reference[:, N]
+            cost_function += cs.sumsqr(reference_cost) + cs.sumsqr(terminal_cost)
+
+        self.optim_prob.minimize(cost_function) # cost function
+
+
+    def integrate(self, state, input):
+        # currently this is only suited for the Differential Drive
+        x_k_next = input[0]*cs.cos(state[2])
+        y_k_next = input[0]*cs.sin(state[2])
+        theta_k_next = input[1]
+        return cs.vertcat(x_k_next, y_k_next, theta_k_next)
+
+    def add_constraints(self):
+        self.optim_prob.subject_to(self.X[:,0] == self.x0)
+        for k in range(self.N):
+            self.optim_prob.subject_to(self.X[:,k+1] == self.X[:,k] + self.delta_t*self.integrate(self.X[:,k], self.U[:,k]))
+        if self.goal is not None:
+            self.optim_prob.subject_to(self.X[:,self.N] == self.goal)
+
+    def simulation(self):
+        simulation_time = np.zeros(self.N_simul)
+        for k in range(self.N_simul):
+            iter_time = time.time()
+            self.optim_prob.set_value(self.x0, self.x[:,k])
+            solution = self.optim_prob.solve()
+
+            self.u[:,k] = solution.value(self.U[:,0]) # only take the initial control from the prediction in the current horizon
+            # these are the trajectories that goes from the current k to k+N (what I predict will happen N steps in the future from now)
+            u_horizon = solution.value(self.U)
+            x_horizon = solution.value(self.X)
+
+            self.optim_prob.set_initial(self.X, x_horizon)
+            self.optim_prob.set_initial(self.U, u_horizon)
+
+            self.x[:, k+1] = self.x[:,k] + self.delta_t*self.integrate(self.x[:,k], self.u[:,k]).full().squeeze(-1)
+            simulation_time[k] = time.time() - iter_time
+
+        print('The average computation time is: ', np.mean(iter_time) * 1000, ' ms')
+
+    def plot(self):
+        plt.plot(0, 0, marker='o', color="cornflowerblue", label="Start")
+        if self.goal is not None:
+            plt.plot(self.goal[0], self.goal[1], marker='o', color="darkorange", label="Goal")
+        if self.reference is not None:
+            plt.plot(self.reference[0,:], self.reference[1,:], color="yellowgreen", label="Reference Trajectory")
+        plt.plot(self.x[0,:], self.x[1,:], color="mediumpurple", label="Predicted Trajectory")
+        plt.legend()
+        plt.show()      
+
+
+delta_t = 0.01
+mpc = Mpc(n=3, m=2, N=40, N_simul=300, sampling_time=delta_t, goal=(-2,-3,-cs.pi/2))
+mpc.add_constraints()
+mpc.simulation()
+mpc.plot()
+
+# psi = np.linspace(0, 2*np.pi, int(1/delta_t))
+# reference = np.array([[1+np.cos(psi)],[2+np.sin(psi)]]).squeeze(1)
+# mpc = Mpc(n=3, m=2, N=40, N_simul=300, sampling_time=delta_t, reference=reference)
+# mpc.add_constraints()
+# mpc.simulation()
+# mpc.plot()
+
