aeeedffdf6edd1343ec3e33d3e69b92631ad7d37
Salvatore Rago <salvatorerago373@gmail.com>
2024-12-16 20:25:53 +0100

    Humanoid2DAnim: Implemented update() with offline trajectory

--- Source/humanoid_2d_animation.py
+++ Source/humanoid_2d_animation.py
@@ -21,7 +21,8 @@ class HumanoidAnimationHelper(BaseAnimationHelper):
     # The length of the tick in the line representing the motion of the CoM
     TICK_LENGTH = 1
 
-    def __init__(self, start_conf, goal_conf, fig=None, ax=None):
+    def __init__(self, start_conf, goal_conf, fig=None, ax=None, following_com_position=None, following_left_foot=None,
+                 following_right_foot=None):
         """
         Initializes a new instance of the utility to show the animation regarding the humanoid.
 
@@ -32,6 +33,11 @@ class HumanoidAnimationHelper(BaseAnimationHelper):
         is created.
         :param ax: The second element returned by plt.subplots(). If either fig or axis is not provided, a new one
         is created.
+        :param following_com_position: The CoM positions starting from the configuration after the start configuration.
+        :param following_left_foot: The left foot positions starting from the configuration after the start
+         configuration.
+        :param following_right_foot: The right foot positions starting from the configuration after the start
+         configuration
         """
         super(HumanoidAnimationHelper, self).__init__(fig, ax)
         # Put the start and goal configurations in a numpy vector (and put the orientation in the interval [0, 2pi] rad)
@@ -54,6 +60,10 @@ class HumanoidAnimationHelper(BaseAnimationHelper):
         self.left_foot_history = [left_foot]
         self.right_foot_history = [right_foot]
 
+        # Compute the following CoM and feet position
+        self.following_com_position = following_com_position
+        self.following_left_foot = following_left_foot
+        self.following_right_foot = following_right_foot
         # The number of the previous frame
         self.last_seen_frame = -1
 
@@ -103,8 +113,9 @@ class HumanoidAnimationHelper(BaseAnimationHelper):
 
     def show_animation_with_autogeneration(self, path_to_gif: str, num_frames: int = 20, interval: int = 200):
         """
-        Shows the animation regarding the humanoid and saves it at save_path. Only the start and goal
-         configuration can be provided: the following configurations will be automatically computed.
+        Shows the animation regarding the humanoid and saves it at save_path. All the configurations
+         following the start and goal configurations will be computed automatically after each frame.
+
         :param path_to_gif: The path to the GIF file where the animation will be saved.
         :param num_frames: The number of frames in the animation.
         :param interval: Delay between frames in milliseconds.
@@ -112,6 +123,23 @@ class HumanoidAnimationHelper(BaseAnimationHelper):
         ani = FuncAnimation(self.fig, self.update_with_autogeneration, frames=num_frames, interval=interval)
         ani.save(path_to_gif, writer='ffmpeg')
         plt.show()
+
+    def show_animation_with_offline_trajectory(self, path_to_gif: str, num_frames: int = 20, interval: int = 200):
+        """
+        Shows the animation regarding the humanoid and saves it at save_path. The configurations
+        following the start and goal configurations are the ones stored in self.following_com_position,
+        self.following_left_foot and self.following_right_foot.
+
+        :param path_to_gif: The path to the GIF file where the animation will be saved.
+        :param num_frames: The number of frames in the animation.
+        :param interval: Delay between frames in milliseconds.
+        """
+        ani = FuncAnimation(self.fig, self.update_with_offline_trajectory, frames=num_frames, interval=interval)
+        ani.save(path_to_gif, writer='ffmpeg')
+        plt.show()
+
+    def show_animation(self, path_to_gif: str, num_frames: int = 20, interval: int = 200):
+        self.show_animation_with_offline_trajectory(path_to_gif, num_frames, interval)
 
     def _draw_circle(self, position, alpha=1.0, radius=2.0, color='tomato', fill=True, linewidth=2):
         robot = plt.Circle((position[0], position[1]), radius=radius, color=color,
@@ -125,8 +153,8 @@ class HumanoidAnimationHelper(BaseAnimationHelper):
 
     def update_with_autogeneration(self, frame):
         """
-        Updates the canvas to display the state corresponding to the given frame number. Only the start and goal
-         configuration can be provided: the following configurations will be automatically computed.
+        Updates the canvas to display the state corresponding to the given frame number. All the configurations
+         following the start and goal configurations will be computed automatically after each frame.
 
         :param frame: The number of the frame whom state has to be represented.
         """
@@ -170,6 +198,79 @@ class HumanoidAnimationHelper(BaseAnimationHelper):
         ax = plt.gca()
         ax.set_aspect('equal', adjustable='box')
 
+    def update_with_offline_trajectory(self, frame):
+        """
+        Updates the canvas to display the state corresponding to the given frame number. The configurations
+        following the start and goal configurations are the ones stored in self.following_left_foot,
+        self.following_left_foot and self.following_right_foot.
+
+        :param frame: The number of the frame whom state has to be represented.
+        """
+        res = self.update(frame)
+        if res == -1:
+            return
+
+        # ===== COMPUTING NEXT VALUES =====
+        self.com_pose_history.append(self.following_com_position.pop(0))
+        foot = 0 if frame % 2 == 0 else 1
+        if foot == 0:
+            print("LEFT")
+            self.left_foot_history.append(self.following_left_foot.pop(0))
+        else:
+            print("RIGHT")
+            self.right_foot_history.append(self.following_right_foot.pop(0))
+
+        plt.xlim(HumanoidAnimationHelper.MIN_COORD, HumanoidAnimationHelper.MAX_COORD)
+        plt.ylim(HumanoidAnimationHelper.MIN_COORD, HumanoidAnimationHelper.MAX_COORD)
+        ax = plt.gca()
+        ax.set_aspect('equal', adjustable='box')
+
+
+def generate_com_and_feet_evolution(start_com, start_left_foot, start_right_foot, num_frames):
+    """
+    Generates all the CoM and feet positions of the animation (starting from the configuration after the start
+     configuration).
+    """
+    com_pose_history = []
+    left_foot_history = []
+    right_foot_history = []
+
+    last_conf = start_com
+    last_left_foot = start_left_foot
+    last_right_foot = start_right_foot
+    for i in range(num_frames):
+        # Set a lower step size for the first step
+        step_size = STEP_SIZE / 2 if i == 0 else STEP_SIZE
+
+        # Generate the new CoM configuration
+        next_conf = np.array([
+            last_conf[0] + step_size / 2 * np.cos(last_conf[2]),
+            last_conf[1] + step_size / 2 * np.sin(last_conf[2]),
+            last_conf[2]
+        ])
+        com_pose_history.append(next_conf)
+        last_conf = next_conf
+
+        foot = 0 if i % 2 == 0 else 1
+        if foot == 0:
+            # Generate the new left feet position
+            new_left_foot = np.array([
+                last_left_foot[0] + step_size * np.cos(next_conf[2]),
+                last_left_foot[1] + step_size * np.sin(next_conf[2])
+            ])
+            left_foot_history.append(new_left_foot)
+            last_left_foot = new_left_foot
+        else:
+            # Generate the new right feet position
+            new_right_foot = np.array([
+                last_right_foot[0] + step_size * np.cos(next_conf[2]),
+                last_right_foot[1] + step_size * np.sin(next_conf[2])
+            ])
+            right_foot_history.append(new_right_foot)
+            last_right_foot = new_right_foot
+
+    return com_pose_history, left_foot_history, right_foot_history
+
 
 if __name__ == "__main__":
     # Set a random goal
@@ -178,4 +279,10 @@ if __name__ == "__main__":
     start = np.random.randint(-10, 10, (3, 1))
 
     anim_helper = HumanoidAnimationHelper(goal_conf=goal, start_conf=start)
-    anim_helper.show_animation_with_autogeneration('../Assets/Animations/humanoid_2d_animation.gif')
+    # anim_helper.show_animation_with_autogeneration('../Assets/Animations/humanoid_2d_animation.gif')
+
+    anim_helper.following_com_position, anim_helper.following_left_foot, anim_helper.following_right_foot = (
+        generate_com_and_feet_evolution(
+            anim_helper.start, anim_helper.left_foot_history[0],
+            anim_helper.right_foot_history[0], num_frames=21))
+    anim_helper.show_animation('../Assets/Animations/humanoid_2d_animation.gif')
