77654313e00b3e026e9120cba4d25e9fef0db6d2
EugenioBugli <eugeniobugli15@gmail.com>
2024-12-16 16:15:27 +0100

    fixes but still no tracking

--- Source/mpc.py
+++ Source/mpc.py
@@ -18,7 +18,7 @@ class Mpc():
         p_opts, s_opts = {"ipopt.print_level": 0, "expand": True}, {}
         self.optim_prob.solver("ipopt", p_opts, s_opts) # define the solver as ipopt (NLP solver)
 
-        # predicted trajectory over the simulation
+        # predicted trajectory over the simulation (not something inside mpc)
         self.u = np.zeros((self.control_dim, self.N_simul))
         self.x = np.zeros((self.state_dim, self.N_simul+1))
 
@@ -28,17 +28,6 @@ class Mpc():
 
         # initial state (this value will be update at each step)
         self.x0 = self.optim_prob.parameter(self.state_dim)
-
-        cost_function = cs.sumsqr(self.U)
-        if self.reference is not None:
-            reference_cost = 0
-            for k in range(self.N):
-                reference_cost += self.x[1:, k] - self.reference[:, k] # xy trajectory
-            terminal_cost = self.x[1:, N] - self.reference[:, N]
-            cost_function += cs.sumsqr(reference_cost) + cs.sumsqr(terminal_cost)
-
-        self.optim_prob.minimize(cost_function) # cost function
-
 
     def integrate(self, state, input):
         # currently this is only suited for the Differential Drive
@@ -55,6 +44,7 @@ class Mpc():
             self.optim_prob.subject_to(self.X[:,self.N] == self.goal)
 
     def simulation(self):
+        self.cost_function()
         simulation_time = np.zeros(self.N_simul)
         for k in range(self.N_simul):
             iter_time = time.time()
@@ -72,7 +62,20 @@ class Mpc():
             self.x[:, k+1] = self.x[:,k] + self.delta_t*self.integrate(self.x[:,k], self.u[:,k]).full().squeeze(-1)
             simulation_time[k] = time.time() - iter_time
 
-        print('The average computation time is: ', np.mean(iter_time) * 1000, ' ms')
+        print('The average computation time is: ', np.mean(simulation_time) * 1000, ' ms')
+
+    def cost_function(self):
+        cost_function = cs.sumsqr(self.U)
+
+        if self.reference is not None:
+            reference_cost = 0
+            for k in range(self.N): # change this everytime look from k to k+N
+                reference_cost += cs.sumsqr(self.X[:2, k] - self.reference[:, k]) # xy trajectory
+            terminal_cost = self.X[:2, self.N] - self.reference[:, self.N]
+            weight = 100
+            cost_function += weight*reference_cost + weight*cs.sumsqr(terminal_cost)
+
+        self.optim_prob.minimize(cost_function) # cost function
 
     def plot(self):
         plt.plot(0, 0, marker='o', color="cornflowerblue", label="Start")
@@ -81,20 +84,21 @@ class Mpc():
         if self.reference is not None:
             plt.plot(self.reference[0,:], self.reference[1,:], color="yellowgreen", label="Reference Trajectory")
         plt.plot(self.x[0,:], self.x[1,:], color="mediumpurple", label="Predicted Trajectory")
+        print(self.x)
         plt.legend()
         plt.show()      
 
 
 delta_t = 0.01
-mpc = Mpc(n=3, m=2, N=40, N_simul=300, sampling_time=delta_t, goal=(-2,-3,-cs.pi/2))
-mpc.add_constraints()
-mpc.simulation()
-mpc.plot()
-
-# psi = np.linspace(0, 2*np.pi, int(1/delta_t))
-# reference = np.array([[1+np.cos(psi)],[2+np.sin(psi)]]).squeeze(1)
-# mpc = Mpc(n=3, m=2, N=40, N_simul=300, sampling_time=delta_t, reference=reference)
+# mpc = Mpc(n=3, m=2, N=10, N_simul=300, sampling_time=delta_t, goal=(4,1.5,cs.pi/2))
 # mpc.add_constraints()
 # mpc.simulation()
 # mpc.plot()
 
+N_simul = 300
+psi = np.linspace(0, 2*np.pi, N_simul)
+reference = np.array([[-1+np.cos(psi)],[np.sin(psi)]]).squeeze(1)
+mpc = Mpc(n=3, m=2, N=10, N_simul=N_simul, sampling_time=delta_t, reference=reference)
+mpc.add_constraints()
+mpc.simulation()
+mpc.plot()
