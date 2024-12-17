d23f95e84dc9835b5376cb39304ee1c5e6fa040f
EugenioBugli <eugeniobugli15@gmail.com>
2024-12-17 20:28:37 +0100

    clean mpc

--- Source/mpc.py
+++ Source/mpc.py
@@ -9,60 +9,63 @@ class Mpc():
         self.state_dim = n
         self.control_dim = m
         self.N = N # horizon MPC 
-        self.N_simul = N_simul # simulation
+        self.N_simul = N_simul # simulation steps
         self.delta_t = sampling_time
         self.goal = goal
-        self.reference = reference
+
+        # trajectory obtained after the simulation (is not something inside the mpc)
+        self.u = np.zeros((self.control_dim, self.N_simul))
+        self.x = np.zeros((self.state_dim, self.N_simul+1))
 
         self.optim_prob = cs.Opti()
         p_opts, s_opts = {"ipopt.print_level": 0, "expand": True}, {}
         self.optim_prob.solver("ipopt", p_opts, s_opts) # define the solver as ipopt (NLP solver)
 
-        # predicted trajectory over the simulation (not something inside mpc)
-        self.u = np.zeros((self.control_dim, self.N_simul))
-        self.x = np.zeros((self.state_dim, self.N_simul+1))
-
-        # trajectory predicted during each step from the actual state to the horizon
-        self.X = self.optim_prob.variable(self.state_dim, self.N+1)
-        self.U = self.optim_prob.variable(self.control_dim, self.N) 
+        # trajectory predicted by the mpc (from k to k+N)
+        self.X_mpc = self.optim_prob.variable(self.state_dim, self.N+1)
+        self.U_mpc = self.optim_prob.variable(self.control_dim, self.N) 
 
         # initial state (this value will be update at each step)
         self.x0 = self.optim_prob.parameter(self.state_dim)
-        self.reference = self.optim_prob.parameter(self.state_dim-1, self.N)
+        # reference trajectory (at each simulation step k we take the reference from k to k+N)
+        if goal is None:
+            self.reference = self.optim_prob.parameter(self.state_dim-1, self.N)
+        else :
+            self.reference = None
 
         self.add_constraints()
         self.cost_function()
 
     def integrate(self, state, input):
-        # currently this is only suited for the Differential Drive
+        # currently this is only suited for the Differential Drive (Euler Integration)
         x_k_next = input[0]*cs.cos(state[2])
         y_k_next = input[0]*cs.sin(state[2])
         theta_k_next = input[1]
         return cs.vertcat(x_k_next, y_k_next, theta_k_next)
 
     def add_constraints(self):
-        self.optim_prob.subject_to(self.X[:,0] == self.x0)
+        self.optim_prob.subject_to(self.X_mpc[:,0] == self.x0)
         for k in range(self.N):
-            self.optim_prob.subject_to(self.X[:,k+1] == self.X[:,k] + self.delta_t*self.integrate(self.X[:,k], self.U[:,k]))
-        # if self.goal is not None:
-        #     self.optim_prob.subject_to(self.X[:,self.N] == self.goal)
+            self.optim_prob.subject_to(self.X_mpc[:,k+1] == self.X_mpc[:,k] + self.delta_t*self.integrate(self.X_mpc[:,k], self.U_mpc[:,k]))
+        if self.goal is not None:
+            self.optim_prob.subject_to(self.X_mpc[:,self.N] == self.goal)
 
-    def simulation(self, ref):
+    def simulation(self, ref=None):
         simulation_time = np.zeros(self.N_simul)
         for k in range(self.N_simul):
             iter_time = time.time()
             self.optim_prob.set_value(self.x0, self.x[:,k])
-            if self.reference is not None:
+            if ref is not None:
                 self.optim_prob.set_value(self.reference, ref[:,k:k+self.N])
             solution = self.optim_prob.solve()
 
-            self.u[:,k] = solution.value(self.U[:,0]) # only take the initial control from the prediction in the current horizon
+            self.u[:,k] = solution.value(self.U_mpc[:,0]) # only take the initial control from the prediction in the current horizon
             # these are the trajectories that goes from the current k to k+N (what I predict will happen N steps in the future from now)
-            u_horizon = solution.value(self.U)
-            x_horizon = solution.value(self.X)
+            u_horizon = solution.value(self.U_mpc)
+            x_horizon = solution.value(self.X_mpc)
 
-            self.optim_prob.set_initial(self.X, x_horizon)
-            self.optim_prob.set_initial(self.U, u_horizon)
+            self.optim_prob.set_initial(self.X_mpc, x_horizon)
+            self.optim_prob.set_initial(self.U_mpc, u_horizon)
 
             self.x[:, k+1] = self.x[:,k] + self.delta_t*self.integrate(self.x[:,k], self.u[:,k]).full().squeeze(-1)
             simulation_time[k] = time.time() - iter_time
@@ -71,12 +74,12 @@ class Mpc():
         self.plot(ref)
 
     def cost_function(self):
-        cost_function = cs.sumsqr(self.U)
+        cost_function = cs.sumsqr(self.U_mpc)
         if self.reference is not None: # change the condition later
             reference_cost = 0
             for k in range(self.N-1): # change this everytime look from k to k+N
-                reference_cost += cs.sumsqr(self.X[:2, k] - self.reference[:, k]) # xy trajectory
-            terminal_cost = cs.sumsqr(self.X[:2, -2] - self.reference[:, -1])
+                reference_cost += cs.sumsqr(self.X_mpc[:2, k] - self.reference[:, k]) # xy trajectory
+            terminal_cost = cs.sumsqr(self.X_mpc[:2, -2] - self.reference[:, -1])
             weight = 500
             cost_function += weight*reference_cost + weight*terminal_cost
         self.optim_prob.minimize(cost_function) # cost function
@@ -91,30 +94,25 @@ class Mpc():
         plt.legend()
         plt.show()      
 
-goal = (3,1.5,cs.pi/2)
+# uncomment this to do posture regulation
+goal = (-3,1.5,cs.pi/2)
 delta_t = 0.01
-# mpc = Mpc(n=3, m=2, N=10, N_simul=300, sampling_time=delta_t, goal=goal)
-# mpc.add_constraints()
-# mpc.simulation()
-# mpc.plot()
+mpc = Mpc(n=3, m=2, N=10, N_simul=300, sampling_time=delta_t, goal=goal)
+mpc.simulation()
 
-N_simul = 1000
-N = 30
-k = 1.2
-alpha_x = k*np.cos(goal[2]) - 3*goal[0]
-beta_x = k*np.cos(goal[2])
-alpha_y = k*np.cos(goal[2]) - 3*goal[1]
-beta_y = k*np.cos(goal[2])
-
-s = np.linspace(0, 1, N_simul+N+1)
-x_traj = goal[0]*s**3 - alpha_x*(s-1)*s**2 + beta_x*s*(s-1)**2
-y_traj = goal[1]*s**3 - alpha_x*(s-1)*s**2 + beta_y*s*(s-1)**2
-reference = np.array([x_traj, y_traj])
-mpc = Mpc(n=3, m=2, N=N, N_simul=N_simul, sampling_time=delta_t)
-mpc.simulation(ref=reference)
-
-# TODO
-# fix cost function
-# change structure and names for x,u and X,U
-# add some comments
-# create a base class and then start with the Humanoid
+# uncomment this to do trajectory tracking
+# goal = (-3,1.5,cs.pi/2)
+# delta_t = 0.01
+# N_simul = 1000
+# N = 30
+# k = 1.2
+# alpha_x = k*np.cos(goal[2]) - 3*goal[0]
+# beta_x = k*np.cos(goal[2])
+# alpha_y = k*np.cos(goal[2]) - 3*goal[1]
+# beta_y = k*np.cos(goal[2])
+# s = np.linspace(0, 1, N_simul+N+1)
+# x_traj = goal[0]*s**3 - alpha_x*(s-1)*s**2 + beta_x*s*(s-1)**2
+# y_traj = goal[1]*s**3 - alpha_x*(s-1)*s**2 + beta_y*s*(s-1)**2
+# reference = np.array([x_traj, y_traj])
+# mpc = Mpc(n=3, m=2, N=N, N_simul=N_simul, sampling_time=delta_t)
+# mpc.simulation(ref=reference)
