6d85b6b7dff9676e210643934a1116a7f05eaed3
Salvatore Rago <salvatorerago373@gmail.com>
2025-01-15 20:51:25 +0100

    Cost fun as fun of U

--- HumanoidNavigation/MPC/HumanoidMPC2.py
+++ HumanoidNavigation/MPC/HumanoidMPC2.py
@@ -1,14 +1,10 @@
 import time
-from abc import ABC
 
-import matplotlib
-import numpy as np
 import casadi as cs
+import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
 from matplotlib.patches import Rectangle
-from sympy.diffgeom.rn import theta
-
-from BaseMpc import MpcSkeleton
 
 from HumanoidNavigation.Utils.obstacles_no_sympy import generate_random_convex_polygon, plot_polygon
 
@@ -22,7 +18,6 @@ FOOT_RECT_HEIGHT = 0.2
     humanoid control: [f_x, f_y, omega] = [stance_foot_pos_x, stance_foot_pos_y, turning_rate]
 """
 # SALVO DELTA_T = 1e-3
-# DELTA_T = 1  # SALVO
 DELTA_T = 0.4  # SALVO
 GRAVITY_CONST = 9.81
 COM_HEIGHT = 1
@@ -71,7 +66,7 @@ class HumanoidMPC:
         self.precomputed_theta = None
         # An array of constants such that the i-th element is 1 if the right foot is the stance at time instant i,
         # -1 if the stance is the left foot.
-        self.s_v = [0 for _ in range(self.N_simul)]
+        # self.s_v = [0 for _ in range(self.N_simul)]
 
         # to be sure they are defined
         assert (self.goal is not None and self.obstacles is not None)
@@ -80,11 +75,11 @@ class HumanoidMPC:
         self.control_dim = 2
 
         # Define the state and the control variables (without theta and omega)
-        self.X_mpc = self.optim_prob.variable(self.state_dim, self.N_horizon)
+        self.X_mpc = self.optim_prob.variable(self.state_dim, self.N_horizon + 1)
         self.U_mpc = self.optim_prob.variable(self.control_dim, self.N_horizon)
         self.x0 = self.optim_prob.parameter(self.state_dim)
         # Define the parameters for theta and omega (that will be precomputed)
-        self.X_mpc_theta = self.optim_prob.parameter(1, self.N_horizon)
+        self.X_mpc_theta = self.optim_prob.parameter(1, self.N_horizon + 1)
         self.U_mpc_omega = self.optim_prob.parameter(1, self.N_horizon)
         self.x0_theta = self.optim_prob.parameter(1)
 
@@ -105,8 +100,7 @@ class HumanoidMPC:
         # we are pre-computing the heading angle as the direction from the current position towards the goal position
         omega_max = 0.156 * cs.pi  # unit: rad/s
         omega_min = -omega_max
-        # target_heading_angle = cs.atan2(self.goal[1] - start_state[2], self.goal[0] - start_state[0])
-        target_heading_angle = cs.atan2(self.goal[1] - start_state[2], self.goal[0] - start_state[0]) + cs.pi  # SALVO
+        target_heading_angle = cs.atan2(self.goal[1] - start_state[2], self.goal[0] - start_state[0])
 
         # omegas (turning rate)
         self.precomputed_omega = [
@@ -117,7 +111,7 @@ class HumanoidMPC:
 
         # thetas (heading angles)
         self.precomputed_theta = [start_state_theta]  # initial theta
-        for k in range(self.N_horizon - 1):
+        for k in range(self.N_horizon):
             self.precomputed_theta.append(
                 self.precomputed_theta[-1] + self.precomputed_omega[k] * self.sampling_time
             )
@@ -127,38 +121,37 @@ class HumanoidMPC:
         self.optim_prob.subject_to(self.X_mpc[:, 0] == self.x0)
 
         # goal constraint (only in position)
-        # self.optim_prob.subject_to(self.X_mpc[0, self.N_horizon-1] == self.goal[0])
-        # self.optim_prob.subject_to(self.X_mpc[2, self.N_horizon-1] == self.goal[2])
+        self.optim_prob.subject_to(self.X_mpc[0, self.N_horizon] == self.goal[0])
+        self.optim_prob.subject_to(self.X_mpc[2, self.N_horizon] == self.goal[2])
 
         # horizon constraint (via dynamics)
-        for k in range(self.N_horizon - 1):
-            state_res, theta_res = self.integrate(self.X_mpc[:, k], self.U_mpc[:, k],
-                                                  self.X_mpc_theta[k], self.U_mpc_omega[k])
-            self.optim_prob.subject_to(
-                cs.eq(
-                    cs.vertcat(self.X_mpc[:, k + 1], self.X_mpc_theta[k + 1]),
-                    cs.vertcat(state_res, theta_res)
-                )
-            )
+        for k in range(self.N_horizon):
+            integration_res = self.integrate(self.X_mpc[:, k], self.U_mpc[:, k],
+                                             self.X_mpc_theta[k], self.U_mpc_omega[k])
+            self.optim_prob.subject_to(cs.eq(self.X_mpc[:, k + 1], integration_res))
+            # self.optim_prob.subject_to(
+            #     cs.eq(
+            #         cs.vertcat(self.X_mpc[:, k + 1], self.X_mpc_theta[k + 1]),
+            #         cs.vertcat(state_res, theta_res)
+            #     )
+            # )
 
             # leg reachability -> prevent the over-extension of the swing leg
-            # reachability = self.leg_reachability(self.X_mpc[:, k], self.X_mpc_theta[k])
-            # self.optim_prob.subject_to(cs.le(reachability, cs.vertcat(L_MAX, L_MAX)))
-            # self.optim_prob.subject_to(cs.ge(reachability, cs.vertcat(-L_MAX, -L_MAX)))
+            reachability = self.leg_reachability(self.X_mpc[:, k], self.X_mpc_theta[k])
+            self.optim_prob.subject_to(cs.le(reachability, cs.vertcat(L_MAX, L_MAX)))
+            self.optim_prob.subject_to(cs.ge(reachability, cs.vertcat(-L_MAX, -L_MAX)))
 
             # walking velocities constraint
-            local_velocities = self.walking_velocities(self.X_mpc[:, k + 1], self.X_mpc_theta[k], k)
-            self.optim_prob.subject_to(local_velocities <= V_MAX)
-            self.optim_prob.subject_to(local_velocities >= V_MIN)
+            # local_velocities = self.walking_velocities(self.X_mpc[:, k + 1], self.X_mpc_theta[k], k)
+            # self.optim_prob.subject_to(local_velocities <= V_MAX)
+            # self.optim_prob.subject_to(local_velocities >= V_MIN)
 
             # maneuverability constraint (right now using the same v_max of the walking constraint)
-            velocity_term, turning_term = self.maneuverability(self.X_mpc[:, k], self.X_mpc_theta[k],
-                                                               self.U_mpc_omega[k])
-            self.optim_prob.subject_to(velocity_term <= turning_term)
+            # velocity_term, turning_term = self.maneuverability(self.X_mpc[:, k], self.X_mpc_theta[k],
+            #                                                    self.U_mpc_omega[k])
+            # self.optim_prob.subject_to(velocity_term <= turning_term)
 
-            # TODO probably missing constraint related to the foot stance (u[0], u[1]) ~ zmp wrt to com
-
-        # # control barrier functions constraint
+        # control barrier functions constraint
         # for k in range(self.N_horizon):
         #     ldcbf_constraints = self.compute_ldcbf(self.X_mpc[:, k], self.obstacles)
         #
@@ -169,11 +162,20 @@ class HumanoidMPC:
         # control actions cost
         # control_cost = cs.sumsqr(cs.vertcat(self.U_mpc, self.U_mpc_omega))
         control_cost = cs.sumsqr(self.U_mpc)
+
         # (p_x - g_x)^2 + (p_y - g_y)^2 distance of a sequence of predicted states from the goal position
-        distance_cost = (cs.sumsqr(self.X_mpc[0, 1:] - cs.DM.ones(1, self.N_horizon - 1) * self.goal[0])
-                         + cs.sumsqr(self.X_mpc[2, 1:] - cs.DM.ones(1, self.N_horizon - 1) * self.goal[2]))
-        # self.optim_prob.minimize(distance_cost + control_cost)
+        # distance_cost = (cs.sumsqr(self.X_mpc[0, :-1] - cs.DM.ones(1, self.N_horizon) * self.goal[0])
+        #                  + cs.sumsqr(self.X_mpc[2, :-1] - cs.DM.ones(1, self.N_horizon) * self.goal[2]))
+
+        # Compute state_N
+        distance_cost = cs.vertcat(0)
+        for k in range(self.N_horizon):
+            state_kp1 = self.integrate(self.X_mpc[:, k], self.U_mpc[:, k],
+                                       self.X_mpc_theta[k], self.U_mpc_omega[k])
+            distance_cost += cs.power(state_kp1[0] - self.goal[0], 2) + cs.power(state_kp1[2] - self.goal[2], 2)
+
         self.optim_prob.minimize(distance_cost)
+        # self.optim_prob.minimize(distance_cost + control_cost)
 
     def integrate(self, x_k, u_k, theta_k, omega_k):
         """
@@ -195,14 +197,14 @@ class HumanoidMPC:
         )
         Bl2 = DELTA_T
 
-        return (Al1 @ x_k) + (Bl1 @ u_k), Al2 * theta_k + Bl2 * omega_k
+        # return (Al1 @ x_k) + (Bl1 @ u_k), Al2 * theta_k + Bl2 * omega_k
+        return (Al1 @ x_k) + (Bl1 @ u_k)
 
     def plot(self, X_pred, U_pred):
         fix, ax = plt.subplots()
 
         # Plot the start position
-        start_state = self.optim_prob.value(self.x0)
-        plt.plot(start_state[0], start_state[2], marker='o', color="cornflowerblue", label="Start")
+        plt.plot(X_pred[0, 0], X_pred[2, 0], marker='o', color="cornflowerblue", label="Start")
 
         # Plot the goal position
         plt.plot(self.goal[0], self.goal[2], marker='o', color="darkorange", label="Goal")
@@ -220,7 +222,7 @@ class HumanoidMPC:
 
             # Create rectangle centered at the position
             rect = Rectangle((-FOOT_RECT_WIDTH / 2, -FOOT_RECT_HEIGHT / 2), FOOT_RECT_WIDTH, FOOT_RECT_HEIGHT,
-                             color='blue' if self.s_v[time_instant] == 1 else 'green', alpha=0.7)
+                             color='blue' if time_instant % 2 == 0 else 'green', alpha=0.7)
             # Apply rotation
             t = (matplotlib.transforms.Affine2D().rotate(foot_orient) +
                  matplotlib.transforms.Affine2D().translate(step_x, step_y) + ax.transData)
@@ -232,8 +234,8 @@ class HumanoidMPC:
         plt.legend()
         # plt.xlim(-1 - self.goal[0], self.goal[0] + 1)
         # plt.ylim(-1 - self.goal[2], self.goal[2] + 1)
-        plt.xlim(-10, 12)
-        plt.ylim(-10, 12)
+        plt.xlim(-5, 7)
+        plt.ylim(-2, 12)
         plt.show()
 
     def simulation(self):
@@ -250,8 +252,9 @@ class HumanoidMPC:
 
             # precompute compute theta and omega
             self.parameters_precalculation(X_pred[:4, k], X_pred[4, k])
-            for i in range(self.N_horizon):
+            for i in range(self.N_horizon + 1):
                 self.optim_prob.set_value(self.X_mpc_theta[i], self.precomputed_theta[i])
+            for i in range(self.N_horizon):
                 self.optim_prob.set_value(self.U_mpc_omega[i], self.precomputed_omega[i])
 
             # solve
@@ -275,14 +278,15 @@ class HumanoidMPC:
             # print(kth_solution.value(self.U_mpc))
 
             # assign to X_mpc and U_mpc the relative values
-            self.optim_prob.set_initial(self.X_mpc, kth_solution.value(self.X_mpc))
+            # self.optim_prob.set_initial(self.X_mpc, kth_solution.value(self.X_mpc))
             self.optim_prob.set_initial(self.U_mpc, kth_solution.value(self.U_mpc))
 
             # compute x_k_next using x_k and u_k
-            state_res, theta_res = self.integrate(X_pred[:4, k], U_pred[:2, k],
-                                                  self.precomputed_theta[0], self.precomputed_omega[0])
+            state_res = self.integrate(X_pred[:4, k], U_pred[:2, k],
+                                       self.precomputed_theta[0], self.precomputed_omega[0])
             X_pred[:4, k + 1] = state_res.full().squeeze(-1)
-            X_pred[4, k + 1] = theta_res
+            # assign X_mpc to the relative value
+            self.optim_prob.set_initial(self.X_mpc[:, 1], X_pred[:4, k + 1])
 
             computation_time[k] = time.time() - starting_iter_time  # CLOCK
 
@@ -292,7 +296,6 @@ class HumanoidMPC:
     # ===== PAPER-SPECIFIC CONSTRAINTS =====
     def walking_velocities(self, x_k_next, theta_k, k):
         s_v = 1 if k % 2 == 0 else -1  # s_v = 1 right foot, s_v = -1 left foot
-        self.s_v[k] = s_v
 
         local_velocities = cs.vertcat(
             cs.cos(theta_k) * x_k_next[1] + cs.sin(theta_k) * s_v * x_k_next[3],
@@ -360,9 +363,9 @@ if __name__ == "__main__":
 
     mpc = HumanoidMPC(
         N_horizon=3,
-        N_simul=100,
+        N_simul=150,
         sampling_time=DELTA_T,
-        goal=(1, 0, 10, 0, 0),  # position=(4, 0), velocity=(0, 0) theta=0
+        goal=(3, 0, 0, 0, 0),  # position=(4, 0), velocity=(0, 0) theta=0
         obstacles=obstacles
     )
 
