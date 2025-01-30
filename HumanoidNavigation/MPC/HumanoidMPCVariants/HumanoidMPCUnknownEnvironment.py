aeecaf8de2ee2ef7771e0724f165b56f576d16ce
Salvatore Rago <salvatorerago373@gmail.com>
2025-01-30 12:09:20 +0100

    Update HumanoidMPCUnknownEnvironment.py

--- HumanoidNavigation/MPC/HumanoidMPCVariants/HumanoidMPCUnknownEnvironment.py
+++ HumanoidNavigation/MPC/HumanoidMPCVariants/HumanoidMPCUnknownEnvironment.py
@@ -6,4 +6,8 @@ class HumanoidMPCUnknownEnvironment(HumanoidMPC):
     A subclass of HumanoidMPC, where the robot is not aware of the full map, but it can only perceive the environment
      through a LiDAR system.
     """
-    pass
+
+    def _get_list_c_and_eta(self, loc_x_k: float, loc_y_k: float, glob_theta_k: float, glob_x_k: float,
+                            glob_y_k: float):
+        # TODO: make changes here
+        return super()._get_list_c_and_eta(loc_x_k, loc_y_k, glob_theta_k, glob_x_k, glob_y_k)
