9a6eddd1031512cd2b225333e48b7f1800af1167
Salvatore Rago <salvatorerago373@gmail.com>
2025-02-09 17:41:57 +0100

    Implemented PlotUtils

--- HumanoidNavigation/report_simulations/simulation_1.py
+++ HumanoidNavigation/report_simulations/simulation_1.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from HumanoidNavigation.MPC.HumanoidMpc import HumanoidMPC, conf
 from HumanoidNavigation.Utils.ObstaclesUtils import ObstaclesUtils
+from HumanoidNavigation.Utils.PlotsUtils import PlotUtils
 from HumanoidNavigation.Utils.obstacles import generate_obstacles, set_seed
 
 PLOTS_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__)))) + "/Assets/Simulation1/"
@@ -45,7 +46,13 @@ def run_simulation_1():
         verbosity=0
     )
 
-    mpc.run_simulation(path_to_gif=f'{PLOTS_PATH}/animation.gif', make_fast_plot=True)
+    X_pred_glob, U_pred_glob = mpc.run_simulation(path_to_gif=f'{PLOTS_PATH}/animation.gif', make_fast_plot=True)
+
+    PlotUtils.plot_signals([
+        (X_pred_glob[[0, 2], :] - np.array([[goal[0]], [goal[1]]]), "Position error$", ['X error', 'Y error']),
+        (np.expand_dims(X_pred_glob[4, :], axis=0), "Orientation $\\theta$"),
+        (np.expand_dims(U_pred_glob[2, :], axis=0), "Turning rate $\\omega$")
+    ], path_to_pdf=f"{PLOTS_PATH}/evolutions.pdf")
 
 
 if __name__ == "__main__":
