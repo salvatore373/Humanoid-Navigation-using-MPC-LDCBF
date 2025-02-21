77bcc81b19cee66bc57dde9fbe492e6a51a863f9
Salvatore Rago <salvatorerago373@gmail.com>
2025-02-21 10:51:20 +0100

    PlotUtils: replaced simulation steps with sec

--- HumanoidNavigation/Utils/PlotsUtils.py
+++ HumanoidNavigation/Utils/PlotsUtils.py
@@ -8,7 +8,7 @@ class PlotUtils:
     """
 
     @staticmethod
-    def plot_signals(data_tuples: list[tuple], path_to_pdf: str):
+    def plot_signals(data_tuples: list[tuple], path_to_pdf: str, samples_per_second: float = None):
         """
         Prints a vertical stack of plots. The i-th plot contains the data at position i in data_tuples.
 
@@ -16,6 +16,8 @@ class PlotUtils:
          the second one is the label to put on the y-axis; the third one is optional, and is the label to assign to the
           data in each row of the matrix. The content of each tuple is plotted separately.
         :params path_to_pdf: The path where the plot will be stored as a PDF.
+        :params samples_per_second: The number of elements belonging to the same second in the array to show on
+         the Y-axis.
         """
         n_plots = len(data_tuples)
         # Create one subplot per tuple, stacked vertically.
@@ -27,12 +29,13 @@ class PlotUtils:
         for i, tpl in enumerate(data_tuples):
             matrix, ylabel_str = tpl[0], tpl[1]
             ax = axes[i]
-            steps = np.arange(matrix.shape[1])
+            steps = np.arange(matrix.shape[1]) if samples_per_second is None else \
+                np.arange(matrix.shape[1]) / samples_per_second
             # Plot each row as a separate signal.
             for j in range(matrix.shape[0]):
                 ax.plot(steps, matrix[j, :], label=None if len(tpl) == 2 else tpl[2][j])
             ax.set_ylabel(ylabel_str)
-            ax.set_xlabel("Simulation Step k")
+            ax.set_xlabel("Simulation Step k" if samples_per_second is None else "Time (s)")
             ax.legend()
             ax.grid(True)
 
--- HumanoidNavigation/report_simulations/simulation_1.py
+++ HumanoidNavigation/report_simulations/simulation_1.py
@@ -19,14 +19,16 @@ def run_simulation_1():
     initial_state = (0, 0, 0, 0, 0)
     goal_pos = (5, 5)
 
+    sampling_time = conf['DELTA_T']  # 1e-1
+    num_steps_per_second = 1 / sampling_time
+
     _, _, obstacles = Scenario.load_scenario(Scenario.BASE, start=(initial_state[0], initial_state[2]),
                                              goal=goal_pos, seed=7)
 
     mpc = HumanoidMPC(
         N_horizon=3,
         N_mpc_timesteps=300,
-        sampling_time=conf["DELTA_T"],
-        # sampling_time=1e-1,
+        sampling_time=sampling_time,
         goal=goal_pos,
         init_state=initial_state,
         obstacles=obstacles,
@@ -42,7 +44,7 @@ def run_simulation_1():
         (X_pred_glob[[1, 3], :], "Translational velocity", ['X velocity', 'Y velocity']),
         (np.expand_dims(X_pred_glob[4, :], axis=0), "Orientation $\\theta$"),
         (np.expand_dims(U_pred_glob[2, :], axis=0), "Turning rate $\\omega$")
-    ], path_to_pdf=f"{PLOTS_PATH_BASE}/evolutions.pdf")
+    ], path_to_pdf=f"{PLOTS_PATH_BASE}/evolutions.pdf", samples_per_second=num_steps_per_second)
 
     # anim.plot_animation(path_to_gif=f'{PLOTS_PATH}/animation.gif')
     anim.plot_animation(path_to_gif=f'{PLOTS_PATH_BASE}/animation.gif',
@@ -57,14 +59,16 @@ def run_simulation_circles():
     initial_state = (0, 0, 3, 0, 0)
     goal_pos = (6, -3)
 
+    sampling_time = conf['DELTA_T']  # 1e-1
+    num_steps_per_second = 1 / sampling_time
+
     _, _, obstacles = Scenario.load_scenario(Scenario.CIRCLE_OBSTACLES, start=(initial_state[0], initial_state[2]),
                                              goal=goal_pos)
 
     mpc = HumanoidMPC(
         N_horizon=3,
         N_mpc_timesteps=300,
-        sampling_time=conf["DELTA_T"],
-        # sampling_time=1e-1,
+        sampling_time=sampling_time,
         goal=goal_pos,
         init_state=initial_state,
         obstacles=obstacles,
@@ -80,7 +84,7 @@ def run_simulation_circles():
         (X_pred_glob[[1, 3], :], "Translational velocity", ['X velocity', 'Y velocity']),
         (np.expand_dims(X_pred_glob[4, :], axis=0), "Orientation $\\theta$"),
         (np.expand_dims(U_pred_glob[2, :], axis=0), "Turning rate $\\omega$")
-    ], path_to_pdf=f"{PLOTS_PATH_CIRCLES}/evolutions.pdf")
+    ], path_to_pdf=f"{PLOTS_PATH_CIRCLES}/evolutions.pdf", samples_per_second=num_steps_per_second)
 
     # anim.plot_animation(path_to_gif=f'{PLOTS_PATH_CIRCLES}/animation.gif')
     anim.plot_animation(path_to_gif=f'{PLOTS_PATH_CIRCLES}/animation.gif',
@@ -88,5 +92,5 @@ def run_simulation_circles():
 
 
 if __name__ == "__main__":
-    # run_simulation_1()
+    run_simulation_1()
     run_simulation_circles()
