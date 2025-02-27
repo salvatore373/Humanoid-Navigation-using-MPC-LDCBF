6b325640dfca171d42c4a98769f0447bec300974
Salvatore Rago <salvatorerago373@gmail.com>
2025-02-27 18:20:05 +0100

    Implemented sim1circles custom LDCBF and uploaded figures

--- HumanoidNavigation/report_simulations/simulation_1.py
+++ HumanoidNavigation/report_simulations/simulation_1.py
@@ -2,6 +2,7 @@ import os
 
 import numpy as np
 
+from HumanoidNavigation.MPC.HumanoidMPCVariants.HumanoidMPCCustomLCBF import HumanoidMPCCustomLCBF
 from HumanoidNavigation.MPC.HumanoidMPCVariants.HumanoidMPCUnknownEnvironment import HumanoidMPCUnknownEnvironment
 from HumanoidNavigation.MPC.HumanoidMpc import conf, HumanoidMPC
 from HumanoidNavigation.Utils.ObstaclesUtils import ObstaclesUtils
@@ -13,6 +14,8 @@ PLOTS_PATH_BASE = os.path.dirname(
     os.path.dirname(os.path.dirname(os.path.realpath(__file__)))) + "/Assets/ReportResults/Simulation1"
 PLOTS_PATH_CIRCLES = os.path.dirname(
     os.path.dirname(os.path.dirname(os.path.realpath(__file__)))) + "/Assets/ReportResults/Simulation1Circles"
+PLOTS_PATH_CIRCLES_DELTA = os.path.dirname(
+    os.path.dirname(os.path.dirname(os.path.realpath(__file__)))) + "/Assets/ReportResults/Simulation1CirclesDelta"
 PLOTS_PATH_UNK_ENV = os.path.dirname(
     os.path.dirname(os.path.dirname(os.path.realpath(__file__)))) + "/Assets/ReportResults/Simulation1UnkEnv"
 PLOTS_PATH_UNK_ENV2 = os.path.dirname(
@@ -97,6 +100,49 @@ def run_simulation_circles():
     anim.plot_animation(path_to_gif=f'{PLOTS_PATH_CIRCLES}/animation.gif',
                         path_to_frames_folder=f'{PLOTS_PATH_CIRCLES}/grid_frames')
 
+def run_simulation_circles_custom_ldcbf():
+    """
+    It runs the circles simulation with custom LDCBF described in paragraph 1 of the Simulation chapter, and generates
+    all the associated plots.
+    """
+    initial_state = (0, 0, 3, 0, 0)
+    goal_pos = (6, -3)
+
+    sampling_time = conf['DELTA_T']  # 1e-1
+    num_steps_per_second = 1 / sampling_time
+
+    delta = 0.3
+
+    _, _, obstacles = Scenario.load_scenario(Scenario.CIRCLE_OBSTACLES, start=(initial_state[0], initial_state[2]),
+                                             goal=goal_pos)
+
+    mpc = HumanoidMPCCustomLCBF(
+        N_horizon=3,
+        N_mpc_timesteps=300,
+        sampling_time=sampling_time,
+        goal=goal_pos,
+        init_state=initial_state,
+        obstacles=obstacles,
+        verbosity=0,
+        distance_from_obstacles=delta,
+    )
+
+    X_pred_glob, U_pred_glob, anim = mpc.run_simulation(path_to_gif=f'{PLOTS_PATH_CIRCLES_DELTA}/animation.gif',
+                                                        make_fast_plot=True,
+                                                        plot_animation=False, fill_animator=True)
+
+    PlotUtils.plot_signals([
+        (X_pred_glob[[0, 2], :] - np.array([[goal_pos[0]], [goal_pos[1]]]), "Position error", ['X error', 'Y error']),
+        (X_pred_glob[[1, 3], :], "Translational velocity", ['X velocity', 'Y velocity']),
+        (np.expand_dims(X_pred_glob[4, :], axis=0), "Orientation $\\theta$"),
+        (np.expand_dims(U_pred_glob[2, :], axis=0), "Turning rate $\\omega$")
+    ], path_to_pdf=f"{PLOTS_PATH_CIRCLES_DELTA}/evolutions", samples_per_second=num_steps_per_second)
+
+    anim.delta = delta
+    # anim.plot_animation(path_to_gif=f'{PLOTS_PATH_CIRCLES_DELTA}/animation.gif')
+    anim.plot_animation(path_to_gif=f'{PLOTS_PATH_CIRCLES_DELTA}/animation.gif',
+                        path_to_frames_folder=f'{PLOTS_PATH_CIRCLES_DELTA}/grid_frames')
+
 
 def run_simulation_unk_env():
     """
@@ -157,4 +203,5 @@ def run_simulation_unk_env():
 if __name__ == "__main__":
     # run_simulation_1()
     # run_simulation_circles()
-    run_simulation_unk_env()
+    # run_simulation_unk_env()
+    run_simulation_circles_custom_ldcbf()
